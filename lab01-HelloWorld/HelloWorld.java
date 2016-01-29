9225b014a29bf6045e8925ee317fa7cf018b6db3
egu218 <egu218@lehigh.edu>
2016-01-29 17:01:21 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+// Eddison Ugaddan
+// Demo 0
+// Hello World Java Program
+
+//  define a class
+public class HelloWorld{
+    
+ 
+//  add a main method
+    public static void main(String[] args) {
+//  print the statement with a box around it   
+    System.out.println("#####################");
+    System.out.println("#                   #");
+    System.out.println("# Hello World 2016! #");
+    System.out.println("#                   #");
+    System.out.println("#####################");
+    System.out.println("test");
+        
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+}
